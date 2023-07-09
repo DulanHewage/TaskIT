ce4fa323d538bbf08496d01468c4bc5e8a70c4a4
Dulan Hewage <dulanhewage2@gmail.com>
2023-07-09 12:38:59 +0530

    Initial commit
    Configure SCSS
    Add default layout
    Configure tailwindcss

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <TheHeader />
+    <div
+      class="flex-auto max-w-4xl min-w-0 pt-6 lg:px-8 lg:pt-8 pb:12 xl:pb-24 lg:pb-16 m-auto"
+    >
+      <Nuxt />
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-lg text-center">TaskIT</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
