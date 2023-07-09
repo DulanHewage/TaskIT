a34689f241954ce267a4e00130896e518497760e
Dulan Hewage <dulanhewage2@gmail.com>
2023-07-09 18:21:39 +0530

    Create BaseButton component
    Add tests for BaseButton component

---
+++ components/Base/BaseButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    class="font-medium rounded-lg text-sm px-5 py-2.5 mr-1"
+    :class="typeColorClasses"
+    v-bind="$attrs"
+    @click="$emit('click')"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    secondary: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    typeColorClasses() {
+      if (this.secondary) {
+        return " text-slate-600 bg-slate-100 hover:bg-slate-200 focus:ring-slate-300";
+      }
+      return "text-white bg-teal-700 hover:bg-teal-800 focus:ring-teal-300";
+    },
+  },
+};
+</script>
---
+++ test/BaseButton.spec.js
@@ -0,0 +1,43 @@
+import { mount } from "@vue/test-utils";
+import BaseButton from "@/components/Base/BaseButton.vue";
+
+describe("BaseButton", () => {
+  test("renders without errors", () => {
+    const wrapper = mount(BaseButton);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  test("renders with default classes when secondary prop is false", () => {
+    const wrapper = mount(BaseButton);
+    const button = wrapper.find("button");
+
+    expect(button.classes()).toContain("text-white");
+    expect(button.classes()).toContain("bg-teal-700");
+    expect(button.classes()).toContain("hover:bg-teal-800");
+    expect(button.classes()).toContain("focus:ring-teal-300");
+  });
+
+  test("renders with secondary classes when secondary prop is true", () => {
+    const wrapper = mount(BaseButton, {
+      propsData: {
+        secondary: true,
+      },
+    });
+    const button = wrapper.find("button");
+
+    expect(button.classes()).toContain("text-slate-600");
+    expect(button.classes()).toContain("bg-slate-100");
+    expect(button.classes()).toContain("hover:bg-slate-200");
+    expect(button.classes()).toContain("focus:ring-slate-300");
+  });
+
+  test("emits a click event when button is clicked", async () => {
+    const wrapper = mount(BaseButton);
+    const button = wrapper.find("button");
+    // Trigger a click event
+    await button.trigger("click");
+
+    // Verify that the click event is emitted
+    expect(wrapper.emitted("click")).toBeTruthy();
+  });
+});
