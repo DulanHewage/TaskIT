7c2ed60a5a83874895b18165ccca6d01381849d7
Dulan Hewage <dulanhewage2@gmail.com>
2023-07-14 22:29:11 +0530

    Add due date functionality

--- components/TheToDo.vue
+++ components/TheToDo.vue
@@ -16,30 +16,54 @@
         2
       </div>
     </div>
-    <div class="pb-4">
+    <div class="pb-2">
       <TheTask
         v-for="task in $store.getters.allTasks"
         :key="task.id"
         :task="task"
         @delete="(taskId) => toggleTaskDeleteConfirmation(taskId, true)"
       />
-      <div class="px-4 mt-4 flex items-center">
-        <form @submit.prevent="addTask" class="w-full">
-          <BaseInput
-            id="task-text"
-            v-model="taskText"
-            placeholder="Add a task"
-            class="w-full"
+      <div class="px-4 mt-5">
+        <div class="flex items-center">
+          <form class="w-full" @submit.prevent="addTask">
+            <BaseInput
+              id="task-text"
+              v-model="taskText"
+              placeholder="Add a task"
+              class="w-full"
+            />
+          </form>
+          <BaseButton class="ml-2" circle @click="addTask">
+            <SvgIcon
+              type="mdi"
+              :path="mdiPlus"
+              size="20"
+              class="text-white"
+            ></SvgIcon>
+          </BaseButton>
+        </div>
+        <div class="flex items-center mt-2">
+          <div class="flex items-center mr-2">
+            <label for="due-date" class="text-sm text-slate-700 mr-1"
+              >Set a due date
+            </label>
+            <input id="due-date" v-model="isDueDateActive" type="checkbox" />
+          </div>
+          <input
+            v-model="dueDate"
+            type="date"
+            class="text-sm text-teal-600 disabled:text-slate-500"
+            :disabled="!isDueDateActive"
+            @change="setDueDate"
           />
-        </form>
-        <BaseButton class="ml-2" circle @click="addTask">
-          <SvgIcon
-            type="mdi"
-            :path="mdiPlus"
-            size="20"
-            class="text-white"
-          ></SvgIcon>
-        </BaseButton>
+        </div>
+
+        <div
+          class="text-xs text-red-600 italic font-light mt-1"
+          :class="{ 'opacity-0': !showDueDateError }"
+        >
+          due date must be in today or the future
+        </div>
       </div>
     </div>
     <BaseModal
@@ -49,7 +73,10 @@
     >
       <template #footer>
         <div class="flex justify-end items-center w-full">
-          <BaseButton secondary @click="closeTaskModal" class="mr-2"
+          <BaseButton
+            secondary
+            class="mr-2"
+            @click="toggleTaskDeleteConfirmation(null, false)"
             >Cancel</BaseButton
           >
           <BaseButton danger @click="deleteTask(selectedTaskId)"
@@ -74,6 +101,9 @@ export default {
       taskText: "",
       showTaskDeleteModal: false,
       selectedTaskId: null,
+      isDueDateActive: false,
+      dueDate: undefined,
+      showDueDateError: false,
     };
   },
   computed: {
@@ -92,11 +122,13 @@ export default {
           id: nanoid(6),
           text: this.taskText,
           completed: false,
-          dueDate: null,
+          dueDate: this.dueDate && this.isDueDateActive ? this.dueDate : null,
           createdAt: new Date(),
         };
         this.$store.dispatch("addTask", task);
         this.taskText = "";
+        this.dueDate = undefined;
+        this.isDueDateActive = false;
       }
     },
     closeTaskModal() {
@@ -114,6 +146,33 @@ export default {
       } else {
         this.selectedTaskId = null;
         this.showTaskDeleteModal = false;
+      }
+    },
+    setDueDate(e) {
+      // if date is in the past, set to today
+      const today = new Date();
+      const dueDate = new Date(e.target.value);
+      if (dueDate < today) {
+        // prevent default behaviour
+        e.preventDefault();
+        // set due date to today
+        this.dueDate = fromDateToString(today).split("T")[0];
+
+        // show error message
+        this.showDueDateError = true;
+        setTimeout(() => {
+          this.showDueDateError = false;
+        }, 3000);
+      } else {
+        // set due date to selected date
+        this.dueDate = e.target.value;
+      }
+      // get date in local time zone
+      function fromDateToString(date) {
+        date = new Date(+date);
+        date.setTime(date.getTime() - date.getTimezoneOffset() * 60000);
+        const dateAsString = date.toISOString().substr(0, 19);
+        return dateAsString;
       }
     },
   },
